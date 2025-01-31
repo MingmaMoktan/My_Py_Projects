6935b106962f9c18fbf726f648c2a8844e4cbbcb
sb13512 <sb13512@student.samk.fi>
2025-01-31 13:49:53 +0200

    Added some files to learn the modules and packages

---
+++ Understanding_module/first_module.py
@@ -0,0 +1,7 @@
+def say_hi():
+    print("Hi!")
+
+PI = 3.14
+
+if __name__ =="__main__":
+    print("You are not suppose to run this file.")
---
+++ Understanding_module/main.py
@@ -0,0 +1,9 @@
+import first_module
+
+import my_package.my_math as mm
+
+first_module.say_hi()
+print(first_module.PI)
+
+x = mm.add(2,3)
+print(x)
