134a4d4baa58ae09b8f33a2befd1c5f8ff215dcd
sb13512 <sb13512@student.samk.fi>
2025-01-31 14:04:07 +0200

    Created some more packages

--- Understanding_module/main.py
+++ Understanding_module/main.py
@@ -4,8 +4,13 @@ import my_package.my_math as mm
 
 from other.other_stuff import stuff
 
+import other.other_module as om
+
 x = mm.add(2,3)
 print(x)
 
 y = stuff(2,2)
 print(y)
+
+z = om.function_from_other_module()
+print(z)
---
+++ Understanding_module/other/other_module.py
@@ -0,0 +1,2 @@
+def function_from_other_module():
+    print("This is the function from other/other module.")
