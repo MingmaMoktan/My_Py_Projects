9cd5495dd581a8de63116d904e223945271fbf04
sb13512 <sb13512@student.samk.fi>
2025-03-10 18:36:33 +0200

    Made some get endpoints and ran the server throught the ECHO API to test and it worked.

--- WEB API/main.py
+++ WEB API/main.py
@@ -7,3 +7,9 @@ async def root():
     return {
         "message": "Oh, Hello. I am your friendly neighbourhood API!"
     }
+    
+@app.get("/test")
+async def test():
+    return {
+        'test': 'Here is the test function.'
+    }
