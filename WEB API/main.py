adc0a434ad5f4d7473c83303b2a6b59ac99bea00
sb13512 <sb13512@student.samk.fi>
2025-03-10 19:17:07 +0200

    Made some changes on the API code and tested by passing the get parameters

--- WEB API/main.py
+++ WEB API/main.py
@@ -13,3 +13,27 @@ async def test():
     return {
         'test': 'Here is the test function.'
     }
+    
+# /cart/add
+@app.get("/cart/add")
+async def cartAdd(itemName, amount):
+    if not amount.isnumeric(): return {'error': 'Amount should be numeric.'}
+    amount = int(amount)
+    if amount < 0: return {'error': 'Amount should be more than 0.'}
+    
+    # We could add the item to some state, database...
+    
+    return {
+        "message": f"Added {amount} of item {itemName} into cart.",
+        "itemName": itemName,
+        "amount": amount
+    }
+
+@app.get("/cart/remove")
+async def cartRemove(itemName, amount=1):
+    amount = int(amount)
+    return {
+        "message": f"{amount} item {itemName} has been removed.",
+        "itemName": itemName,
+        "amount": amount
+    }
