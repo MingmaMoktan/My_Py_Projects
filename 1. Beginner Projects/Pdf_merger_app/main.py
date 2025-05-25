141ecb30f381bd40d71c7e56af70040d9b20322d
sb13512 <sb13512@student.samk.fi>
2025-05-25 13:40:34 +0300

    Added some comments for those who want to use this app.

--- 1. Beginner Projects/Pdf_merger_app/main.py
+++ 1. Beginner Projects/Pdf_merger_app/main.py
@@ -1,4 +1,14 @@
 # This is my pdf merger app.
+"""
+If you want to use this app you should install the pypdf by using the following command.
+
+pip install pypdf
+
+And the pdf files that you want to merge should be on the same folder your application file is.
+After that you can run the file and enter the number of files you want to merge and enter the names of the pdf files you want to merge. 
+And then this will give you the merged pdf file by the default name in the app. 
+If you want to change the name of the merged pdf file then you can change the name from the app file.
+"""
 from pypdf import PdfWriter
 
 merger = PdfWriter()
