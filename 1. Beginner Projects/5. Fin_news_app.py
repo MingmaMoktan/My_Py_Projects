ed74a20cc8ac4123f146bd0063b9ad1ada2b2d95
sb13512 <sb13512@student.samk.fi>
2025-05-25 14:04:56 +0300

    Added some comments for the instruction who wants to use this app.

--- 1. Beginner Projects/5. Fin_news_app.py
+++ 1. Beginner Projects/5. Fin_news_app.py
@@ -1,10 +1,17 @@
+"""
+Here I have created the simple news app that you can use. 
+
+Please use your own news api key. If you don't have the news api key you can create one by going to this url https://newsapi.org/
+
+After creating the newsAPI key you can paste your API key in the key variable below 'key = "" '. And then run the app to see the news. Right now this only shows the url and title but to read the whole description you can click on the url to go to the particular news page. 
+"""
 import requests
 
 query = input("Enter the topic you want to see news about: ")
 
-date = input("Enter the date in format mm-dd: ")
+date = input("Enter the date in format \'mm-dd\': ")
 
-key = "b3c286f32b2341978eb3ccb0501f0b38"
+key = ""
 
 
 url = f"https://newsapi.org/v2/everything?q={query}&from=2025-{date}&sortBy=publishedAt&apiKey={key}"
