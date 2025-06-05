ea7aa93b6cf130aafc8c7fd6d79523490a2cebd2
sb13512 <sb13512@student.samk.fi>
2025-06-05 09:23:16 +0300

    Deleted the blog app for now.

--- 3. Advanced Projects/project_1/myblog/blog/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.conf import settings
-
-# Create your models here.
-class Tag(models.Model):
-    value = models.TextField(max_length=100)
-    
-    def _str_(self):
-        return self.value
